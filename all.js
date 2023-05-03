f9fb4813b8e08b0742e946b85d6eaba4d42f0990
Kochevnik <sam.kochevnik.uz@gmail.com>
2023-05-03 17:56:15 +0500

    15 lesson ni do konsa

--- all.js
+++ all.js
@@ -8,12 +8,26 @@ const personMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из просмотринных фильм", "");
-const b = prompt("На скоько оцените его?", "");
-const c = prompt("Один из просмотринных фильм", "");
-const d = prompt("На скоько оцените его?", "");
-
-personMovieDB.movies[a] = b;
-personMovieDB.movies[c] = d;
-
+let a, c;
+for(let i = 0; i < personMovieDB.count; i++){
+    a = prompt("Один из просмотринных фильм", "");
+    c = prompt("На скоько оцените его?", "");
+    personMovieDB.movies[a] = c;
+}
 console.log(personMovieDB)
+
+// const a = prompt("Один из просмотринных фильм", "");
+// const b = prompt("На скоько оцените его?", "");
+// const c = prompt("Один из просмотринных фильм", "");
+// const d = prompt("На скоько оцените его?", "");
+
+// personMovieDB.movies[a] = b;
+// personMovieDB.movies[c] = d;
+
+// document.write(personMovieDB)
+
+// let num = 60;
+//
+// while (++num<65) {
+//     console.log(num);
+// }
