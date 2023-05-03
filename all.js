80b9777985f9c1d89608c22878ab30ab09bd4acf
Kochevnik <sam.kochevnik.uz@gmail.com>
2023-05-03 16:47:23 +0500

    lesson 1

--- all.js
+++ all.js
@@ -1,13 +1,19 @@
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(4)
+const personMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-let hi = 10;
-    hello = 10;
+const a = prompt("Один из просмотринных фильм", "");
+const b = prompt("На скоько оцените его?", "");
+const c = prompt("Один из просмотринных фильм", "");
+const d = prompt("На скоько оцените его?", "");
 
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
 
-
-console.log(hi++);
-console.log(hello--);
-
-console.log(++hi);
-console.log(--hello);
+console.log(personMovieDB)
